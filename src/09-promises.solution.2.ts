9fffd33fe303a791b083ef677e75c76c1de06709
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2026-06-22 11:38:18 +0200

    initial commit

---
+++ src/09-promises.solution.2.ts
@@ -0,0 +1,20 @@
+interface LukeSkywalker {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+}
+
+export const fetchLukeSkywalker = async () => {
+  const data = await fetch("https://swapi.py4e.com/api/people/1").then(
+    (res) => {
+      return res.json();
+    }
+  );
+
+  return data as LukeSkywalker;
+};
