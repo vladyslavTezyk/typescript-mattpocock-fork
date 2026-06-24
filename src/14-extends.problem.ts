546ca8c260a20cacb9fb4ff2af72499e93653742
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2026-06-24 15:23:15 +0200

    chore: exos 11 to 14

--- src/14-extends.problem.ts
+++ src/14-extends.problem.ts
@@ -6,20 +6,21 @@ import { Equal, Expect } from "./helpers/type-utils";
  * make it more DRY?
  */
 
-interface User {
+interface Base {
   id: string;
+}
+
+interface User extends Base {
   firstName: string;
   lastName: string;
 }
 
-interface Post {
-  id: string;
+interface Post extends Base {
   title: string;
   body: string;
 }
 
-interface Comment {
-  id: string;
+interface Comment extends Base {
   comment: string;
 }
 
