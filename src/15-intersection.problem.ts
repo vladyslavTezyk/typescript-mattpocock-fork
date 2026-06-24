a741973760ec3f9bd4fb5de46ef042425c3c22d5
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2026-06-24 15:59:19 +0200

    chore: exos 15 & 16

--- src/15-intersection.problem.ts
+++ src/15-intersection.problem.ts
@@ -14,7 +14,12 @@ interface Post {
  * How do we type this return statement so it's both
  * User AND { posts: Post[] }
  */
-export const getDefaultUserAndPosts = (): unknown => {
+
+type getDefaultUserAndPosts = User & {
+  posts: Post[]
+}
+
+export const getDefaultUserAndPosts = (): getDefaultUserAndPosts => {
   return {
     id: "1",
     firstName: "Matt",
